3b00f88b9893ba1b1e19c754d1f24d093fff87b9
alexpagava <alexpagava5gmail@gmail.com>
2024-03-24 02:06:57 +0400

    goa

--- day 1/homework/homework.py
+++ day 1/homework/homework.py
@@ -1,4 +1,11 @@
-name="alex"
+dad_age=int(input("მამა 41 წლისაა"))
+mom_age=int(input("დედა 40 წლისაა"))
+
+if dad_age > mom_age:
+    print(dad_age > mom_age)
+###
+    
+    name="alex"
 age=13
 height=178
 
